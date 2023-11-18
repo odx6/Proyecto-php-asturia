660657a3bbe30f49262ddaaa39336aee3a8c1f4b
Ever <you@example.com>
2023-11-18 11:26:46 -0600

    versio1.15-SE AGREGARON LAS OPCIONES PARA MOSTRAR Y ALGUNAS VALIDACIONES EN TIEMPO REAL PARA CAMPOS UNICOS ANTES DE LA VALIDACION DEL EMAIL

--- taller.sql
+++ taller.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 16-11-2023 a las 00:04:56
+-- Tiempo de generación: 18-11-2023 a las 18:24:23
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -113,18 +113,6 @@ INSERT INTO `empleado_permisos` (`idempleado_permiso`, `idempleado`, `idpermiso`
 (336, 9, 10),
 (337, 9, 11),
 (338, 9, 12),
-(339, 1, 1),
-(340, 1, 2),
-(341, 1, 3),
-(342, 1, 4),
-(343, 1, 5),
-(344, 1, 6),
-(345, 1, 7),
-(346, 1, 8),
-(347, 1, 9),
-(348, 1, 10),
-(349, 1, 11),
-(350, 1, 12),
 (351, 10, 1),
 (352, 10, 2),
 (353, 10, 3),
@@ -148,7 +136,31 @@ INSERT INTO `empleado_permisos` (`idempleado_permiso`, `idempleado`, `idpermiso`
 (371, 11, 9),
 (372, 11, 10),
 (373, 11, 11),
-(374, 11, 12);
+(374, 11, 12),
+(375, 12, 1),
+(376, 12, 2),
+(377, 12, 3),
+(378, 12, 4),
+(379, 12, 5),
+(380, 12, 6),
+(381, 12, 7),
+(382, 12, 8),
+(383, 12, 9),
+(384, 12, 10),
+(385, 12, 11),
+(386, 12, 12),
+(411, 1, 1),
+(412, 1, 2),
+(413, 1, 3),
+(414, 1, 4),
+(415, 1, 5),
+(416, 1, 6),
+(417, 1, 7),
+(418, 1, 8),
+(419, 1, 9),
+(420, 1, 10),
+(421, 1, 11),
+(422, 1, 12);
 
 -- --------------------------------------------------------
 
@@ -293,14 +305,15 @@ CREATE TABLE `tblcatemp` (
 --
 
 INSERT INTO `tblcatemp` (`IDEMP`, `STRNSS`, `STRRFC`, `STRCUR`, `STRNOM`, `STRAPE`, `STRDOM`, `STRLOC`, `STRMUN`, `STREST`, `STRCP`, `STRPAI`, `STRTEL`, `STRCOR`, `STRPWS`, `BITSUS`, `STRIMG`, `CREATE_AT`) VALUES
-(1, '543434', '123', 'SDJK544S44A', 'Administrador', 'Administrador', 'las casa num213', 'santa lucia', 'santa lucia', 'Oaxaca', '71230', 'Mexico', '9152905212', 'admin@admin.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', b'1', 'view/resources/images/default.png', '2023-11-11 10:11:13'),
-(3, '123456', '123456', 'Ranulfos', 'Sanchez', 'Ranulfoss', 'alphant76@hotmail.com', 'kdhsdhk', 'Conocido', 'OAXACA DE JUAREZ', '123456', '123456', '45', '1', '0', b'1', 'view/resources/images/default.png', '2023-11-11 10:11:13'),
-(4, '240', '3234dsdd', 'Eduardo', 'Ramirez Luna', 'lalo4678', 'lalo4678@hotmail.com', 'dhjd', 'Santa Lucia', 'Oaxaca', '95151139', '951139', '139', '1', '0', b'1', 'view/resources/images/default.png', '2023-11-11 10:11:13'),
-(5, '555', '12345', 'Ever', 'Agustin', 'Ever', 'Ever@gmail.com', 'df1df31fd', 'Conocido', 'OAXACA', '9512905212', '9512125212', '1234', '1', '0', b'1', 'view/resources/images/default.png', '2023-11-11 10:11:13'),
-(6, '6', '123456', 'Ramon ', 'valdez ', 'Ramon ', 'Ramon@gmail.com', 'jdjhdjk', 'Conocido', 'OAXACA', '9512905212', '9512125212', '1234', '1', '0', b'1', 'view/resources/images/default.png', '2023-11-11 10:11:13'),
-(9, '12345', '1234', '123', '123', '123', 'lsdjdl', 'a', 'a', 'a', 'a', 'a', 'a', 'Ever@gmail.com', '09361980f210d386bfec673303e03ce617dfc107', b'1', 'view/resources/images/default.png', '2023-11-11 22:38:26'),
-(10, '543434', '123', '123', '123', '123', 'lsdjdl', 'a', 'a', 'a', 'a', 'México', '9515925212', 'Everardo@gmail.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', b'1', 'view/resources/images/default.png', '2023-11-15 16:32:21'),
-(11, '12345678910', '123', '123', '123', '123', 'lsdjdl', 'a', 'a', 'a', 'a', 'México', '9515925212', 'Tomas@gmail.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', b'1', 'view/resources/images/default.png', '2023-11-15 16:42:47');
+(1, '543434', '123', 'SDJK544S44A', 'Administrador', 'Administrador', 'las casa num213', 'santa lucia', 'santa lucia', 'Oaxaca', '71230', 'Mexico', '9152905212', 'admin@admin.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', b'1', 'view/resources/images/1700174068_1529891341_asus.jpg', '2023-11-11 10:11:13'),
+(3, '123456', '123456', 'Ranulfos', 'Sanchez', 'Ranulfoss', 'alphant76@hotmail.com', 'kdhsdhk', 'Conocido', 'OAXACA DE JUAREZ', '123456', '123456', '45', '1', '0', b'1', 'view/resources/images/Default/perfil.png', '2023-11-11 10:11:13'),
+(4, '240', '3234dsdd', 'Eduardo', 'Ramirez Luna', 'lalo4678', 'lalo4678@hotmail.com', 'dhjd', 'Santa Lucia', 'Oaxaca', '95151139', '951139', '139', '1', '0', b'1', 'view/resources/images/Default/perfil.png', '2023-11-11 10:11:13'),
+(5, '555', '12345', 'Ever', 'Agustin', 'Ever', 'Ever@gmail.com', 'df1df31fd', 'Conocido', 'OAXACA', '9512905212', '9512125212', '1234', '1', '0', b'1', 'view/resources/images/Default/perfil.png', '2023-11-11 10:11:13'),
+(6, '6', '123456', 'Ramon ', 'valdez ', 'Ramon ', 'Ramon@gmail.com', 'jdjhdjk', 'Conocido', 'OAXACA', '9512905212', '9512125212', '1234', '1', '0', b'1', 'view/resources/images/Default/perfil.png', '2023-11-11 10:11:13'),
+(9, '12345', '1234', '123', '123', '123', 'lsdjdl', 'a', 'a', 'a', 'a', 'a', 'a', 'Ever@gmail.com', '09361980f210d386bfec673303e03ce617dfc107', b'1', 'view/resources/images/Default/perfil.png', '2023-11-11 22:38:26'),
+(10, '543434', '123', '123', '123', '123', 'lsdjdl', 'a', 'a', 'a', 'a', 'México', '9515925212', 'Everardo@gmail.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', b'1', 'view/resources/images/Default/perfil.png', '2023-11-15 16:32:21'),
+(11, '12345678910', '123', '123', '123', '123', 'lsdjdl', 'a', 'a', 'a', 'a', 'México', '9515925212', 'Tomas@gmail.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', b'1', 'view/resources/images/Default/perfil.png', '2023-11-15 16:42:47'),
+(12, '22345678911', 'GOMJ80082556', 'AUCE970202HOCGRV03', 'Q1212', 'Agustin', 'flores magon #206', 'Benito juarez ', 'santa cruz xoxocotlan ', 'Oaxaca', '71230', 'Mexico', '9515925212', 'Oropeza@gmail.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', b'1', 'view/resources/images/Default/perfil.png', '2023-11-16 16:38:08');
 
 -- --------------------------------------------------------
 
@@ -327,10 +340,10 @@ CREATE TABLE `tblcatpro` (
 --
 
 INSERT INTO `tblcatpro` (`STRSKU`, `STRCOD`, `STRDESPRO`, `INTIDSBC`, `MONPCOS`, `INTIDUNI`, `STRIMG`, `INTTIPUSO`, `BITSUS`, `INTIDCAT`, `CREATE_AT`) VALUES
-('10005631120', 'MororV9', 'Motor 7 cilindros', 1, 45, 4, 'view/resources/images/Productos/1699660066_1699658450_1696311214_spaceship.gif', 1, 1, 1, '2023-11-11 12:59:26'),
+('10005631120', 'MororV8', 'Motor 9 cilindros', 1, 4000, 5, 'view/resources/images/Productos/1700236694_1700069730_vehiculo.png', 1, 1, 3, '2023-11-11 12:59:26'),
 ('10005631121', 'MororV8', 'Motor 6 cilindros', 1, 20, 20, 'view/resources/images/Productos/1699660333_1699658749_1696311214_spaceship.gif', 1, 1, 1, '2023-11-11 12:59:26'),
-('10005631122', 'MororV8', 'Motor 6 cilindros', 1, 4, 0, 'view/resources/images/default.png', 1, 1, 1, '2023-11-11 12:59:26'),
-('10005631123', 'MororV8', 'Motor 6 cilindros', 1, 4, 0, 'view/resources/images/default.png', 1, 1, 1, '2023-11-11 12:59:26'),
+('10005631122', 'MororV8', 'Motor 6 cilindros', 3, 4, 5, 'view/resources/images/Productos/1700177723_1700177584_1700172608_1529891341_asus.jpg', 1, 1, 5, '2023-11-11 12:59:26'),
+('10005631123', 'MororV8', 'Motor 6 cilindros', 1, 4, 5, 'view/resources/images/default.png', 1, 1, 3, '2023-11-11 12:59:26'),
 ('10005631124', 'MororV8', 'Motor 6 cilindros', 1, 40000, 4, 'view/resources/images/default.png', 1, 1, 1, '2023-11-11 12:59:26'),
 ('10005631125', 'MororV8', 'Motor 6 cilindros', 1, 40000, 4, 'view/resources/images/Productos/1699463977_spaceship.gif', 1, 1, 1, '2023-11-11 12:59:26'),
 ('10005631126', 'MororV8', 'Motor 6 cilindros', 1, 40000, 4, 'view/resources/images/Productos/1699657173_1529891341_asus.jpg', 1, 1, 1, '2023-11-11 12:59:26'),
@@ -338,10 +351,14 @@ INSERT INTO `tblcatpro` (`STRSKU`, `STRCOD`, `STRDESPRO`, `INTIDSBC`, `MONPCOS`,
 ('10005631130', 'bujia346', 'Motor 6 cilindros', 1, 40000, 4, 'view/resources/images/Productos/1699987929_1529654251_19665339_1547135995331118_8694108703655374046_n.jpg', 1, 1, 3, '2023-11-14 19:52:09'),
 ('10005631132', 'bujia346', 'Motor 6 cilindros', 1, 40000, 4, 'view/resources/images/Productos/1699916533_vehiculo.png', 1, 1, 3, '2023-11-14 00:02:13'),
 ('10005631192', 'bujia314', 'Motor 6 cilindros', 1, 40000, 5, 'view/resources/images/Productos/1700088421_vehiculo.png', 1, 1, 3, '2023-11-15 23:47:01'),
+('10005631193', 'bujia314', 'Motor 6 cilindros', 1, 40000, 5, '', 1, 1, 3, '2023-11-16 16:13:54'),
+('10005631194', 'bujia314', 'Motor 6 cilindros', 1, 40000, 5, 'view/resources/images/Productos/1700147893_Productos1699463582_spaceship.gif', 2, 1, 3, '2023-11-16 16:18:13'),
+('10005631199', 'llanta 3.5', 'llanta mas grande que la otra ', 3, 40000, 5, '', 2, 1, 5, '2023-11-16 20:12:11'),
 ('1005631110', 'Motor6c', 'BUJIAS NGK PARA COCHE', 1, 35, 1, 'view/resources/images/default.png', 4, 1, 1, '2023-11-11 12:59:26'),
 ('100563111018', 'BUJIAS02', 'BUJIA AGREGADA 12', 1, 45, 1, 'view/resources/images/Productos/1700073503_1699659211_1529891341_asus.jpg', 1, 1, 3, '2023-11-15 19:38:23'),
 ('1006632110', 'BUJIAS02', 'BUJIAS NGK PARA CAMIONETA', 1, 35, 1, 'view/resources/images/default.png', 4, 1, 1, '2023-11-11 12:59:26'),
-('1008672319', 'BUJIAS03', 'BUJIAS NGK PARA TRACTOCAMION', 1, 35, 1, 'view/resources/images/default.png', 4, 1, 1, '2023-11-11 12:59:26'),
+('1008672319', 'BUJIAS03', 'BUJIAS NGK PARA TRACTOCAMION', 3, 40000, 5, '', 2, 1, 5, '2023-11-11 12:59:26'),
+('111111111111', 'michelin324', 'fuerza aerea', 3, 40000, 5, 'view/resources/images/Default/productoDefault.png', 2, 1, 5, '2023-11-16 22:36:13'),
 ('2008672319', 'LLANTA01', 'LLANTA AVANTE 205/70/16', 1, 35, 1, 'view/resources/images/default.png', 4, 1, 1, '2023-11-11 12:59:26'),
 ('2009372319', 'LLANTA03', 'LLANTA GOODYEAR 205/70/16', 1, 35, 1, 'view/resources/images/default.png', 4, 1, 1, '2023-11-11 12:59:26'),
 ('2009472319', 'LLANTA04', 'LLANTA GOODYEAR 205/70/15', 1, 35, 1, 'view/resources/images/default.png', 4, 1, 1, '2023-11-11 12:59:26'),
@@ -512,7 +529,7 @@ ALTER TABLE `configuracion`
 -- AUTO_INCREMENT de la tabla `empleado_permisos`
 --
 ALTER TABLE `empleado_permisos`
-  MODIFY `idempleado_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=375;
+  MODIFY `idempleado_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=423;
 
 --
 -- AUTO_INCREMENT de la tabla `empresa`
@@ -536,7 +553,7 @@ ALTER TABLE `tblcatcat`
 -- AUTO_INCREMENT de la tabla `tblcatemp`
 --
 ALTER TABLE `tblcatemp`
-  MODIFY `IDEMP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `IDEMP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `tblcatsbc`
